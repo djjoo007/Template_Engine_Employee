a7af76290fc789dd7d318e4a6bfcd6c0130a702e
Joo Heredia <djjoo007uw@gmail.com>
2020-08-16 13:42:50 -0700

    add lib class

--- app.js
+++ app.js
@@ -9,6 +9,119 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choice = require("inquirer/lib/objects/choice");
+
+let teamArray = [];
+
+// Prompt for Menue
+function promptMenu() {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "option",
+            message: "What would like to do today?",
+            choices: ["Create Engineer", "Create Manager", "Create Intern", "Build Team"]
+        }
+    ]) .then(response => {
+        if (answer.option === "Create Engineer") {
+            promptEngineer();
+        } else if (answer.option === "Create Manager") {
+            promptManager();
+        } else if (answer.option === "Create Intern") {
+            promptIntern();
+        } else if (answer.option === "Build Team") {
+            promptBuildTeam();
+        }
+    }); 
+}
+
+function promptEngineer() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your first name:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your ID",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub Username?"
+        },
+    ]) .then(answers => {
+        const engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+        teamArray.push(engineer);
+        promptMenu();
+    }) 
+}
+
+function promptManager() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your first name:"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email:"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your ID",
+        },
+        {
+            type: "input",
+            name: "office",
+            message: "Enter your Office Number:"
+        },
+    ]) .then(answers => {
+        const manager = new Manager(answer.name, answer.id, answer.email, answer.office);
+        teamArray.push(manager);
+        promptMenu();
+    }); 
+}
+
+function promptIntern() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your First Name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your Email?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your Employee ID?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the name of your school?"
+        },
+    ]) .then(answers => {
+        const intern = new Intern(answer.name, answer.id, answer.email, answer.school);
+        teamArray.push(school);
+        promptMenu();
+    }); 
+}
+
 
 
 // Write code to use inquirer to gather information about the development team members,
